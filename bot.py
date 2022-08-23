6da4d245cef83623742f2b1316c3033af3999c90
googolmogol <googolmogol@gmail.com>
2022-08-23 10:24:11 +0300

    kek

--- bot.py
+++ bot.py
@@ -1,33 +1,48 @@
-from aiogram import Bot, Dispatcher, executor, types
-from aiogram.types import ContentType
+import logging
+import os
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils.executor import start_webhook
+from aiogram import Bot, types
 
 API_TOKEN = '5410734233:AAF7Mpwsm5R-VZ_XenQWwRUbnkrVHsyHVqo'
 
-# Initialize bot and dispatcher
-bot = Bot(token=API_TOKEN)
+TOKEN = os.getenv('BOT_TOKEN')
+bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
 
+HEROKU_APP_NAME = os.getenv('HEROKU_APP_NAME')
 
-# Срабатывает при запуске бота или команде /start или /help
-@dp.message_handler(commands=['start', 'help'])
-async def send_welcome(message: types.Message):
+# webhook settings
+WEBHOOK_HOST = f'https://{HEROKU_APP_NAME}.herokuapp.com'
+WEBHOOK_PATH = f'/webhook/{TOKEN}'
+WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
 
-    await bot.send_message(message.from_user.id, 'hello')
+# webserver settings
+WEBAPP_HOST = '0.0.0.0'
+WEBAPP_PORT = os.getenv('PORT', default=8000)
 
 
-@dp.callback_query_handler()
-async def inline_kb_answer_callback_handler(query: types.CallbackQuery):
-    pass
+async def on_startup(dispatcher):
+    await bot.set_webhook(WEBHOOK_URL, drop_pending_updates=True)
+
+
+async def on_shutdown(dispatcher):
+    await bot.delete_webhook()
 
 
 @dp.message_handler()
 async def echo(message: types.Message):
-    await bot.send_message(message.from_user.id, 'kek')
+    await message.answer(message.text)
 
-
-@dp.message_handler(content_types=ContentType.ANY)
-async def unknown_message(msg: types.Message):
-    pass
 
 if __name__ == '__main__':
-    executor.start_polling(dp, skip_updates=True)
+    logging.basicConfig(level=logging.INFO)
+    start_webhook(
+        dispatcher=dp,
+        webhook_path=WEBHOOK_PATH,
+        skip_updates=True,
+        on_startup=on_startup,
+        on_shutdown=on_shutdown,
+        host=WEBAPP_HOST,
+        port=WEBAPP_PORT,
+    )
